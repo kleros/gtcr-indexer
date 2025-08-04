51b28f040a88784c406d5caa760e21d5624bf5ea
Harman-singh-waraich <Waraichharman68@gmail.com>
2025-08-04 20:45:21 +0530

    feat: evidence-ipfs-fetch

--- src/mappings/LightGTCRMappings/Evidence.ts
+++ src/mappings/LightGTCRMappings/Evidence.ts
@@ -1,15 +1,21 @@
 import { Evidence, LightGeneralizedTCR } from "generated";
-import { ONE, ZERO } from "../../utils";
+import { extractPath, ONE, ZERO } from "../../utils";
+import { fetchEvidenceData } from "../../utils/ipfs/fetchEvidenceData";
 
 LightGeneralizedTCR.Evidence.handlerWithLoader({
   loader: async ({ event, context }) => {
     const evidenceGroupID =
       event.params._evidenceGroupID.toString() + "@" + event.srcAddress;
 
-    const evidenceGroup = await context.EvidenceGroup.getOrCreate({
-      id: evidenceGroupID,
-      numberOfEvidence: ZERO,
-    });
+    const ipfsHash = extractPath(event.params._evidence);
+
+    const [evidenceGroup, evidenceData] = await Promise.all([
+      context.EvidenceGroup.getOrCreate({
+        id: evidenceGroupID,
+        numberOfEvidence: ZERO,
+      }),
+      context.effect(fetchEvidenceData, { ipfsHash }),
+    ]);
 
     const evidence: Evidence = {
       id: evidenceGroupID + "-" + evidenceGroup.numberOfEvidence.toString(),
@@ -20,6 +26,11 @@ LightGeneralizedTCR.Evidence.handlerWithLoader({
       timestamp: BigInt(event.block.timestamp),
       txHash: event.transaction.hash,
       evidenceGroup_id: evidenceGroup.id,
+      title: evidenceData?.title,
+      name: evidenceData?.name,
+      description: evidenceData?.description,
+      fileURI: evidenceData?.fileURI,
+      fileTypeExtension: evidenceData?.fileTypeExtension,
     };
 
     context.EvidenceGroup.set({
@@ -27,9 +38,6 @@ LightGeneralizedTCR.Evidence.handlerWithLoader({
       numberOfEvidence: evidenceGroup.numberOfEvidence + ONE,
     });
     context.Evidence.set(evidence);
-
-    // TODO: ipfs fetching
   },
-
   handler: async ({ event, context, loaderReturn }) => {},
 });
---
+++ src/utils/ipfs/fetchEvidenceData.ts
@@ -0,0 +1,37 @@
+import { experimental_createEffect, S } from "envio";
+import { tryFetchIpfsFile } from ".";
+
+const evidenceSchema = S.schema({
+  title: S.optional(S.string),
+  description: S.optional(S.string),
+  name: S.optional(S.string),
+  fileURI: S.optional(S.string),
+  fileTypeExtension: S.optional(S.string),
+});
+
+export const fetchEvidenceData = experimental_createEffect(
+  {
+    name: "fetchEvidenceData",
+    input: {
+      ipfsHash: S.string,
+    },
+    output: S.union([evidenceSchema, S.shape(S.schema(0), (_) => null)]),
+    cache: true,
+  },
+  async ({ input, context }) => {
+    const { ipfsHash } = input;
+
+    try {
+      const data = await tryFetchIpfsFile(ipfsHash, context);
+      context.log.warn(`asd: ${data}`);
+      S.assertOrThrow(data, evidenceSchema);
+
+      return data;
+    } catch (err) {
+      if (err instanceof Error) {
+        context.log.error(`Error fetching Evidence Data: ${err.message}`);
+      }
+      return null;
+    }
+  }
+);
